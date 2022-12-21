d5736eb02d416b9b7575b5296870588d837431c8
Ganesh Ramanathan <ganesh@datazug.ch>
2022-12-21 15:41:24 +0100

    first version blurb

---
+++ java-orgserver-coap/orgserver/src/main/java/interactions/ics/unisg/ch/smartsensing/Launcher.java
@@ -0,0 +1,52 @@
+package interactions.ics.unisg.ch.smartsensing;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
+import org.eclipse.californium.core.config.CoapConfig;
+import org.eclipse.californium.core.network.CoapEndpoint;
+import org.eclipse.californium.elements.config.Configuration;
+import org.eclipse.californium.elements.config.TcpConfig;
+import org.eclipse.californium.elements.config.UdpConfig;
+import org.eclipse.californium.elements.tcp.netty.TcpServerConnector;
+import org.eclipse.californium.elements.util.NetworkInterfacesUtil;
+
+public class Launcher {
+	
+	public static String fileName = "org.xml";
+	
+	static {
+		CoapConfig.register();
+		UdpConfig.register();
+		TcpConfig.register();
+	}
+
+	public static void main(String[] args) {
+		try {
+			
+			// create server
+			boolean udp = true;
+			boolean tcp = false;
+			System.out.println("Working Directory = " + System.getProperty("user.dir"));
+			int port = Configuration.getStandard().get(CoapConfig.COAP_PORT);
+			if (0 < args.length) {
+				tcp = args[0].equalsIgnoreCase("coap+tcp:");
+				if (tcp) {
+					System.out.println("Please Note: the TCP support is currently experimental!");
+				}
+			}
+			if(args != null && args.length > 0) {
+				fileName = args[0];				
+			}
+			
+			System.out.printf("Intializing OrgServer from %s\n", fileName);
+			OrgServer server = new OrgServer(udp, tcp, port);
+			//server.addEndpoints(udp, tcp, port);
+			server.start();
+
+		} catch (SocketException e) {
+			System.err.println("Failed to initialize server: " + e.getMessage());
+		}
+	}
+}
